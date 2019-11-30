5e61231cb9b6498db889b0c20214041ece594087
Strassboom <maddog241996@gmail.com>
2019-11-29 20:02:27 -0500

    Simplefied levelConfig structure. Created tool to make custom maps.

--- itchGames/templates/redrun/devRoom.js
+++ itchGames/templates/redrun/devRoom.js
@@ -1,7 +1,7 @@
-var nodes = {"a":{"position":[50,50],"children":{"b":{"speed":1.0},"c":{"speed":1.0},"d":{"speed":1.0}},"hasPlayer":false,"hasMonster":false},
-            "b":{"position":[100,200],"children":{"a":{"speed":1.0},"e":{"speed":1.0}},"hasPlayer":false,"hasMonster":false},
-            "c":{"position":[50,300],"children":{"a":{"speed":1.0},"d":{"speed":1.0}},"hasPlayer":false,"hasMonster":false},
-            "d":{"position":[400,400],"children":{"a":{"speed":0.5},"c":{"speed":1.0},"e":{"speed":1.0}},"hasPlayer":false,"hasMonster":false},
-            "e":{"position":[200,300],"children":{"b":{"speed":1.0},"d":{"speed":1.0}},"hasPlayer":false,"hasMonster":false}};
+var nodes = {"a":{"position":[50,50],"children":{"b":{"speed":1.0},"c":{"speed":1.0},"d":{"speed":1.0}}},
+            "b":{"position":[100,200],"children":{"a":{"speed":1.0},"c":{"speed":1.0},"e":{"speed":1.0}}},
+            "c":{"position":[50,300],"children":{"a":{"speed":1.0},"b":{"speed":1.0},"d":{"speed":1.0}}},
+            "d":{"position":[400,400],"children":{"a":{"speed":0.5},"c":{"speed":1.0},"e":{"speed":1.0}}},
+            "e":{"position":[200,300],"children":{"b":{"speed":1.0},"d":{"speed":1.0}}}};
 
 var startPoint = "a"
